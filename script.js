92d1a52fd28378dd7ef5fbe1cd5dcca9defb4bb6
ARAVINDHAN <programmeraravindhan@gmail.com>
2026-05-05 10:38:16 +0530

    Initial commit - Digital Clock

---
+++ script.js
@@ -0,0 +1,22 @@
+function updateClock() {
+  let now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  // Add leading zero
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  let time = hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("clock").innerText = time;
+}
+
+// Update every second
+setInterval(updateClock, 1000);
+
+// Run immediately
+updateClock();
